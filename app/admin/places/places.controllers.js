da72d377a0f59ac0a6b654a8c746bd7c9a4480ee
chertpong_h <chertpong_h@cmu.ac.th>
2016-06-02 23:54:13 +0800

    Add places/edit page

--- app/admin/places/places.controllers.js
+++ app/admin/places/places.controllers.js
@@ -1,10 +1,15 @@
 'use strict';
 angular.module('myApp.admin', ['ngRoute'])
   .config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/admin/places', {
-      templateUrl: 'admin/places/list.html',
-      controller: 'AdminPlaceListCtrl'
-    })
+    $routeProvider
+      .when('/admin/places', {
+        templateUrl: 'admin/places/list.html',
+        controller: 'AdminPlaceListCtrl'
+      })
+      .when('/admin/places/:id', {
+        templateUrl: 'admin/places/edit',
+        controller:'AdminPlaceEditDetailCtrl'
+      })
   }])
   .controller('AdminPlaceListCtrl', [
     '$scope', 'Config', 'localStorageService', '$log','$http', '$location',
@@ -26,5 +31,43 @@ angular.module('myApp.admin', ['ngRoute'])
           $scope.isEnabledLoginButton = true;
           $log.debug(err);
         });
+      $scope.goEdit = function(id){
+        $log.debug('[~] ','go to edit page of place id:',id);
+        $location.path('admin/places/'+id);
+      }
+    }])
+  .controller('AdminPlaceEditDetailCtrl', [
+    '$scope', 'Config', 'localStorageService', '$log','$http', '$location','$routeParams','$q','$timeout',
+    function($scope, Config, localStorageService, $log, $http, $location, $routeParams, $q,$timeout) {
+      $scope.place = {};
+      
+      $scope.workingDay = [
+        'Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday','Mon-Sat','Mon-Fri','Everyday',
+        'Everyday except Monday','Everyday except Tuesday','Everyday except Wednesday',
+        'Everyday except Thursday','Everyday except Friday'
+      ];
+      $scope.workingDaySearchText = '';
+      $scope.workingDaySearch = function(searchText){
+        var deferred = $q.defer();
+        var result = $scope.workingDaySearchText ? $scope.workingDay.filter(function(wd){
+          return wd.toLowerCase().indexOf(searchText.toLowerCase()) === 0;
+        }) : $scope.workingDay;
+        $timeout(function () { deferred.resolve( result ); }, 100, false);
+        return deferred.promise;
+      };
+      var path = Config.backendUrl+'/api/places/'+$routeParams.id;
 
+      $http
+        .get(path,{
+          headers:{
+            'X-ACCESS-TOKEN': localStorageService.get('token')
+          }
+        })
+        .then(function(response) {
+          $scope.place = response.data;
+          $log.debug('[+] load place: ', $scope.place.name);
+        })
+        .catch(function(err) {
+          $log.debug('[!]',err);
+        });
     }]);
